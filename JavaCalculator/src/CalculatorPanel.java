00f1040482ae74bedd52e54a39062df0c93e55e0
Carl Baines <carltbaines@hotmail.co.uk>
2025-05-27 20:10:28 +0100

    Added Subtraction, Multiplication, Division Functionality + Created .JAR file
    - Added subtraction, multiplication and division functionality.
    - Added a footer credit JLabel.
    - Changed the program's method of storing inputs to facilitate doubles.
    - Other tweaks/QOL features

--- JavaCalculator/src/CalculatorPanel.java
+++ JavaCalculator/src/CalculatorPanel.java
@@ -5,16 +5,17 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class CalculatorPanel extends JPanel implements ActionListener {
-    // Attributes
+    // Swing Elements
     private JFrame calculatorFrame;
     private JPanel calculatorPanel;
     private JTextField display;
     private JTextField previousDisplay;
     private JPanel numberPad;
+    private JLabel footerLabel;
 
-    // Initialises an arraylist of integers to store the numbers inputted from the
+    // Initialises an arraylist of doubles to store the numbers inputted from the
     // number pad.
-    private ArrayList<Integer> numbers = new ArrayList<>();
+    private ArrayList<Double> numbers = new ArrayList<>();
     private boolean newInput = false;
     private String currentOperator = "";
 
@@ -28,7 +29,8 @@ public class CalculatorPanel extends JPanel implements ActionListener {
         calculatorFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         calculatorFrame.setSize(400, 600);
         calculatorFrame.setResizable(false);
-        calculatorFrame.setLocationRelativeTo(null); // Center the frame on the screen
+        // Centers the frame on the screen
+        calculatorFrame.setLocationRelativeTo(null);
 
         calculatorPanel = new JPanel();
         calculatorPanel.setLayout(new BoxLayout(calculatorPanel, BoxLayout.PAGE_AXIS));
@@ -88,21 +90,72 @@ public class CalculatorPanel extends JPanel implements ActionListener {
         // Add the numberPad to the calculatorPanel
         calculatorPanel.add(numberPad);
 
+        footerLabel = new JLabel("Created by Carl Baines.");
+        footerLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        footerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        footerLabel.setVisible(true);
+
+        calculatorPanel.add(footerLabel);
+
         return calculatorPanel;
     }
 
-    // Method that performs addition calculation.
-    private int addition(ArrayList<Integer> numbers) {
-        int result = 0;
+    // Method that performs addition calculations.
+    private double addition(ArrayList<Double> numbers) {
+        double result = 0;
         // loop through the numbers arraylist.
         // get each number stored at each index.
         // add each number together.
         for (int i = 0; i < numbers.size(); i++) {
             result += numbers.get(i);
         }
+        // System.out.println("Result of addition: " + result);
+        return result;
+    }
 
-        System.out.println("Result of addition: " + result);
+    // Method that performs subtraction calculations
+    private double subtraction(ArrayList<Double> numbers) {
+        double result = 0;
+        // retrieve the number stored at the first index in the arraylist.
+        double subtractNumber = numbers.get(0);
+        // loop through the numbers arraylist from the first index (to skip index 0).
+        // subtract continually from the number.
+        for (int i = 1; i < numbers.size(); i++) {
+            // System.out.println("Number to be subtracted: " + subtractNumber);
+            subtractNumber -= numbers.get(i);
+            // System.out.println("After subtraction: " + subtractNumber);
+            result = subtractNumber;
+        }
+        return result;
+    }
 
+    // Method that performs multiplication calculations.
+    private double multiplication(ArrayList<Double> numbers) {
+        double result = 0;
+        double multiplyNumber = numbers.get(0);
+        for (int i = 1; i < numbers.size(); i++) {
+            // multiply the first number in the numbers arraylist continually
+            // with each number stored in the arraylist from index one, until the arraylist
+            // has been fully traversed.
+            multiplyNumber *= numbers.get(i);
+            result = multiplyNumber;
+        }
+        // System.out.println("Result of multiplication: " + result);
+        return result;
+    }
+
+    // Method that performs division calculations.
+    private double division(ArrayList<Double> numbers) {
+        double result = 0;
+        double divisionNumber = numbers.get(0);
+        for (int i = 1; i < numbers.size(); i++) {
+            // divide from the first number in the numbers arraylist continually
+            // with the values stored in the arraylist from index one, until the arraylist
+            // has been fully traversed.
+            divisionNumber /= numbers.get(i);
+            result = divisionNumber;
+        }
+        // System.out.println("Result of division: " + result);
         return result;
     }
 
@@ -112,15 +165,12 @@ public class CalculatorPanel extends JPanel implements ActionListener {
         if (display.getText().equals("0") || newInput) {
             // Sets the JTextField text to the digit.
             display.setText(digit);
-
             // set the previousDisplay JTextField content to the contents of the
             // previousDisplayText arraylist.
             // adds the digit to the arraylist of strings.
             previousDisplayStrs.add(digit);
-
             // Update the previous display with the method call.
             formatPreviousDisplay(previousDisplayStrs);
-
             // flag for new number is set to false.
             newInput = false;
         } else {
@@ -132,21 +182,18 @@ public class CalculatorPanel extends JPanel implements ActionListener {
     // Method that handles operator inputs.
     private void handleOperatorInputs(String operator, ArrayList<String> previousDisplayStrs) {
         // Stores the current number as a variable by parsing the number stored in the
-        // JTextField, as a string, to an integer.
-        int currentNumber = Integer.parseInt(display.getText());
+        // JTextField, as a string, to a double.
+        double currentNumber = Double.parseDouble(display.getText());
         // adds it to the numbers arraylist.
         numbers.add(currentNumber);
-        System.out.println("Number added to the arraylist: " + currentNumber);
-        System.out.println(numbers.toString());
-
+        // System.out.println("Number added to the arraylist: " + currentNumber);
+        // System.out.println(numbers.toString());
         // sets the current operator.
         currentOperator = operator;
-
         // add the currentOperator to the previousDisplayStrs arraylist of strings.
         previousDisplayStrs.add(currentOperator);
         // Update the previous display with the method call.
         formatPreviousDisplay(previousDisplayStrs);
-
         // flag for new number is set to true.
         newInput = true;
     }
@@ -154,27 +201,50 @@ public class CalculatorPanel extends JPanel implements ActionListener {
     // Method to calculate the result of the calculations made on the number pad.
     private void calculateResult(ArrayList<String> previousDisplayStrs) {
         // Add the current number to the numbers arraylist.
-        int currentNumber = Integer.parseInt(display.getText());
+        double currentNumber = Double.parseDouble(display.getText());
         numbers.add(currentNumber);
-        System.out.println("Number added to the arraylist: " + currentNumber);
-        System.out.println(numbers.toString());
+        // System.out.println("Number added to the arraylist: " + currentNumber);
+        // System.out.println(numbers.toString());
 
         // Calculate the result based on the operator inputted.
-        int result = 0;
-        if (currentOperator.equals("+")) {
-            result = addition(numbers);
+        double result = 0;
+
+        switch (currentOperator) {
+            case "+":
+                result = addition(numbers);
+                break;
+            case "-":
+                result = subtraction(numbers);
+                break;
+            case "*":
+                result = multiplication(numbers);
+                break;
+            case "/":
+                result = division(numbers);
+                break;
+            default:
+                result = 0;
         }
 
+        // Retrieves the result and removes the '.0' from the end
+        // as the result is stored as a double.
+        String resultStr = String.valueOf(result);
+        if (resultStr.endsWith(".0")) {
+            resultStr = resultStr.substring(0, resultStr.length() - 2);
+        }
         // Display the result in the JTextField.
-        display.setText(String.valueOf(result));
+        display.setText(resultStr);
 
         // Display the full calculation you made in the previousDisplay JTextField.
         // e.g. 1 + 2 =
         formatPreviousDisplay(previousDisplayStrs);
 
+        // Adds the equals sign on to the end of the previousDisplay JTextField to
+        // output the full calculation that was made to get the result.
         previousDisplay.setText(previousDisplay.getText() + "=");
 
-        // Clear the numbers arraylist for the next operation.
+        // Clear the numbers arraylist and the previous display strings arraylist for
+        // the next operation.
         numbers.clear();
         previousDisplayStrs.clear();
         newInput = true;
@@ -183,13 +253,15 @@ public class CalculatorPanel extends JPanel implements ActionListener {
     // Method to check if the text of a button that has been clicked on is numeric.
     private boolean isNumeric(String buttonText) {
         try {
-            Integer.parseInt(buttonText);
+            Double.parseDouble(buttonText);
             return true;
         } catch (NumberFormatException e) {
             return false;
         }
     }
 
+    // Method that formats and updates the previousDisplay JTextField on the
+    // calculator's display.
     private void formatPreviousDisplay(ArrayList<String> previousDisplayStrs) {
         String previousDisplayString = "";
         // loops through the arraylist of strings
@@ -211,6 +283,7 @@ public class CalculatorPanel extends JPanel implements ActionListener {
             // Handle CE button
             if (buttonText.equals("CE")) {
                 display.setText("0");
+                previousDisplay.setText("");
                 // clears the numbers arraylist and sets the currentOperator string to empty.
                 numbers.clear();
                 currentOperator = "";
@@ -222,23 +295,34 @@ public class CalculatorPanel extends JPanel implements ActionListener {
             // is numeric.
             boolean numeric = isNumeric(buttonText);
             // if the button text is numeric, we parse the number stored as a string to an
-            // integer.
+            // double.
             if (numeric) {
-                int numTextToInt = Integer.parseInt(buttonText);
+                double numTextToDouble = Double.parseDouble(buttonText);
                 // check if the number is between 0 and 9.
-                if (numTextToInt >= 0 || numTextToInt <= 9) {
+                if (numTextToDouble >= 0 || numTextToDouble <= 9) {
                     // call the method that handles the numeric inputs.
                     numberInputs(buttonText, previousDisplayStrs);
                 }
             }
+
             // Handle operator buttons.
-            else if (buttonText.equals("+")) {
-                handleOperatorInputs(buttonText, previousDisplayStrs);
-            }
-            // Handle equals button
-            else if (buttonText.equals("=")) {
-                // if the equals button is clicked, call the calculateResult method.
-                calculateResult(previousDisplayStrs);
+            switch (buttonText) {
+                case "+":
+                    handleOperatorInputs("+", previousDisplayStrs);
+                    break;
+                case "-":
+                    handleOperatorInputs("-", previousDisplayStrs);
+                    break;
+                case "*":
+                    handleOperatorInputs("*", previousDisplayStrs);
+                    break;
+                case "/":
+                    handleOperatorInputs("/", previousDisplayStrs);
+                    break;
+                // Handle equals button.
+                case "=":
+                    calculateResult(previousDisplayStrs);
+                    break;
             }
         }
     }
